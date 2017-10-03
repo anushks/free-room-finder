ea682911674cdd117bf8a8172ee6aaa2e1b57760
anushks <anushks@users.noreply.github.com>
2017-10-03 13:02:19 +0100

    /

--- scripts2.js
+++ scripts2.js
@@ -15,7 +15,7 @@ var startdatetime = currentdate.getFullYear() + "-"
                 + "+01:00";
         }
   var enddatetime = currentdate.getFullYear() + "-"
-                + "0" + (currentdate.getMonth()+1) +"-"
+                + (currentdate.getMonth()+1) +"-"
                 + currentdate.getDate() + "T"
                 + (currentdate.getHours()+1) + ":00:00"
                 + "+01:00"
